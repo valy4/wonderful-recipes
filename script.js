72a909dfeb8452cee366c212286e0a0c08b04f65
Daniel Ghimpu <smeu.valentina33@gmail.com>
2021-04-09 17:03:36 +0100

    added search-box

---
+++ script.js
@@ -0,0 +1,34 @@
+const searchbox = document.getElementById("search-bar");
+const container = document.getElementById("recipes-container");
+
+searchbox.addEventListener(`keypress`, function (event) {
+  console.log(event);
+  if (event.key == "Enter") {
+    const search = document.getElementById("search-bar").value;
+
+    fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?query=${search}&apiKey=cc7961a8e292403b983f2c208132a9f4`
+    )
+      .then((response) => response.json())
+
+      .then((data) => {
+        console.log(data, "data");
+        // do something with the results
+        const recipeList = data.results;
+        container.innerHTML = "";
+        for (let i = 0; i < recipeList.length; i++) {
+          // paint the recipe
+          let recipe = recipeList[i];
+          const recipeTitle = document.createElement("p");
+          recipeTitle.innerText = recipe.title;
+          container.appendChild(recipeTitle);
+          console.log(recipe);
+
+          const img = document.createElement("img");
+          img.setAttribute("src", recipe.image);
+          container.appendChild(img)
+
+        }
+      });
+  }
+});
